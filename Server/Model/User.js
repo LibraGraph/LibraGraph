562592eac393689c12751b89656919a9b79fde22
vidhvath28 <vidhvathj.2005@gmail.com>
2025-04-03 15:02:40 +0530

    Server side bug resolved

--- Server/Model/User.js
+++ Server/Model/User.js
@@ -5,7 +5,8 @@ const UserSchema = new mongoose.Schema({
   firstName: { type: String },
   lastName: { type: String },  
   externalId: { type: String, unique: true }, 
-  role: { type: String, default: "user" },
+  role: { type: String, default: "user" }
+}, {
   timestamps: true, 
  
 });
