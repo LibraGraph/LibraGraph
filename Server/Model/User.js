0bbaa3bddd3ffb141707b0f02226acc21d6ece62
vidhvath28 <vidhvathj.2005@gmail.com>
2025-04-03 09:12:51 +0530

    maintenance check with the Server Files

--- Server/Model/User.js
+++ Server/Model/User.js
@@ -5,9 +5,8 @@ const UserSchema = new mongoose.Schema({
   firstName: { type: String },
   lastName: { type: String },  
   externalId: { type: String, unique: true }, 
-  role: { type: String, default: "user" }
-}, {
-  timestamps: true
+  role: { type: String, default: "user" },
+  timestamps: true, 
  
 });
 
